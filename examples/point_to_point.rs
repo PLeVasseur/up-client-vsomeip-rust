35c974c1c1a724997b8f6fcb607a629a81f76013
Peter LeVasseur <peter.levasseur@gm.com>
2024-06-07 10:33:35 -0400

    Showed that the point-to-point listener then calling send() and sending back through the Response works as-is. Need to now show how we handle a Request being sent from up-transport-vsomeip-rust and registering a handler for this on the fly.

--- examples/point_to_point.rs
+++ examples/point_to_point.rs
@@ -1,16 +1,55 @@
-use log::error;
+use log::{error, trace, warn};
 use std::env::current_dir;
 use std::fs::canonicalize;
 use std::sync::Arc;
+use protobuf::Enum;
 use tokio::sync::Notify;
 use up_client_vsomeip_rust::UPClientVsomeip;
-use up_rust::{UListener, UMessage, UStatus, UTransport, UUri};
+use up_rust::{UCode, UListener, UMessage, UMessageBuilder, UMessageType, UStatus, UTransport, UUri};
+use up_rust::UMessageType::UMESSAGE_TYPE_UNSPECIFIED;
 
-pub struct PrintingListener;
+pub struct PrintingListener {
+    client: Arc<UPClientVsomeip>
+}
+
+impl PrintingListener {
+    pub fn new(client: Arc<UPClientVsomeip>) -> Self {
+        Self {
+            client
+        }
+    }
+}
+
 #[async_trait::async_trait]
 impl UListener for PrintingListener {
     async fn on_receive(&self, msg: UMessage) {
-        println!("{:?}", msg);
+        println!("Received in point-to-point listener:\n{:?}", msg);
+
+        match msg.attributes.type_.enum_value_or(UMESSAGE_TYPE_UNSPECIFIED) {
+            UMESSAGE_TYPE_UNSPECIFIED => {
+                warn!("Not supported message type: UNSPECIFIED");
+                return;
+            }
+            UMessageType::UMESSAGE_TYPE_PUBLISH => {
+
+            }
+            UMessageType::UMESSAGE_TYPE_REQUEST => {
+                let response_build_res = UMessageBuilder::response_for_request(msg.attributes.as_ref().unwrap()).with_comm_status(UCode::OK.value()).build();
+                let Ok(response_msg) = response_build_res else {
+                    return;
+                };
+                let _ = self.client.send(response_msg).await.inspect_err(|err| {
+                    warn!("Unable to send response: {err:?}");
+                });
+            }
+            UMessageType::UMESSAGE_TYPE_RESPONSE => {
+
+            }
+            UMessageType::UMESSAGE_TYPE_NOTIFICATION => {
+                warn!("Not supported message type: NOTIFICATION");
+                return;
+            }
+        }
     }
 
     async fn on_error(&self, err: UStatus) {
@@ -65,11 +104,12 @@ async fn main() {
         error!("Unable to establish UTransport");
         return;
     };
+    let client = Arc::new(client);
 
     let source = any_from_authority(service_authority_name);
     let sink = any_uuri();
 
-    let printing_listener: Arc<dyn UListener> = Arc::new(PrintingListener);
+    let printing_listener: Arc<dyn UListener> = Arc::new(PrintingListener::new(client.clone()));
     let reg_res = client
         .register_listener(&source, Some(&sink), printing_listener)
         .await;
