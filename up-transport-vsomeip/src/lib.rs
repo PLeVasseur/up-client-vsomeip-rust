c17670c28f5e7e1d757bf44116852be557b37f8f
Peter LeVasseur <peter.levasseur@gm.com>
2024-07-02 17:21:19 -0400

    Add helper functions which are useful for creating UUris, disassembling parts of UUris.

--- up-transport-vsomeip/src/lib.rs
+++ up-transport-vsomeip/src/lib.rs
@@ -11,7 +11,7 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 
-use up_rust::UUID;
+use up_rust::{UUri, UUID};
 
 mod determine_message_type;
 mod rpc_correlation;
@@ -19,8 +19,52 @@ mod vsomeip_offered_requested;
 
 pub mod transport;
 
+// TODO: use function from up-rust when merged
+pub(crate) fn any_uuri() -> UUri {
+    UUri {
+        authority_name: "*".to_string(),
+        ue_id: 0x0000_FFFF,     // any instance, any service
+        ue_version_major: 0xFF, // any
+        resource_id: 0xFFFF,    // any
+        ..Default::default()
+    }
+}
+
+// TODO: upstream into up-rust
+pub(crate) fn any_uuri_fixed_authority_id(authority_name: &AuthorityName, ue_id: UeId) -> UUri {
+    UUri {
+        authority_name: authority_name.to_string(),
+        ue_id: ue_id as u32,
+        ue_version_major: 0xFF, // any
+        resource_id: 0xFFFF,    // any
+        ..Default::default()
+    }
+}
+
+// TODO: upstream this into up-rust
+pub(crate) fn split_u32_to_u16(value: u32) -> (u16, u16) {
+    let most_significant_bits = (value >> 16) as u16;
+    let least_significant_bits = (value & 0xFFFF) as u16;
+    (most_significant_bits, least_significant_bits)
+}
+
+// TODO: upstream this into up-rust
+pub(crate) fn split_u32_to_u8(value: u32) -> (u8, u8, u8, u8) {
+    let byte1 = (value >> 24) as u8;
+    let byte2 = (value >> 16 & 0xFF) as u8;
+    let byte3 = (value >> 8 & 0xFF) as u8;
+    let byte4 = (value & 0xFF) as u8;
+    (byte1, byte2, byte3, byte4)
+}
+
+// TODO: upstream this into up-rust
+pub(crate) fn create_request_id(client_id: ClientId, session_id: SessionId) -> RequestId {
+    ((client_id as u32) << 16) | (session_id as u32)
+}
+
 pub struct UPClientVsomeip {}
 
+type AuthorityName = String;
 type UeId = u16;
 type ClientId = u16;
 type ReqId = UUID;
